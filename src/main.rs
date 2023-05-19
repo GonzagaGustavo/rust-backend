c0ff18e113b694826e07d8bd9c8df75606f52c73
Gustavo Gonzaga <Gustavogonzaga.gg243@gmail.com>
2023-05-19 16:00:33 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use mysql::*;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8000;
 
-    HttpServer::new(|| App::new().service(index).route("/hey", web::get().to(hey)))
-        .bind(("127.0.0.1", port))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(oi)
+            .route("/hey", web::get().to(hey))
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
+#[get("/product")]
+async fn oi() -> impl Responder {
+    HttpResponse::Ok().body("oi.")
+}
+
 async fn hey() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
